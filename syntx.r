5a8ea61356d611651926ceb344e1294d20980808
het875 <hetglathiya875@gmail.com>
2025-03-12 22:08:04 +0530

    added new file R

---
+++ syntx.r
@@ -0,0 +1,9 @@
+#To output text in R, use single or double quotes:
+
+#However, R does have a print() function available if you want to use it. This might be useful if you are familiar with other programming languages, such as Python, which often uses the print() function to output code.
+print("Hello World!")
+
+
+for (x in 1:10) {
+  print(x)
+}
